09eb19a0c07011c63ecedf296722ddbca71fbafe
rar34 <ra.cse34@gmail.com>
2024-08-16 20:31:18 +0600

    get product count from database

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
     })
 
 
+    app.get("/productsCount", async(req, res)=>{
+        const count = await productsCollection.estimatedDocumentCount();
+        res.send({count})
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
